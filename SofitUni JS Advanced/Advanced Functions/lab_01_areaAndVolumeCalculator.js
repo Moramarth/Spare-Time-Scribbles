74939e5b80eaea75cc8699d883b45f5994fc5180
George Lazarov <122516587+Moramarth@users.noreply.github.com>
2024-01-30 23:05:27 +0200

    Create lab_01_areaAndVolumeCalculator.js
    Added folder for the advanced functions chapter

---
+++ SofitUni JS Advanced/Advanced Functions/lab_01_areaAndVolumeCalculator.js
@@ -0,0 +1,20 @@
+function areaAndVolumeCalculator(areaFunc, volumeFunc, input) {
+    const output = []
+    let x, y, z;
+
+    JSON.parse(input).map(obj => {
+        console.log(obj)
+        x = obj.x
+        y = obj.y
+        z = obj.z
+
+        const boundAreaFunc = areaFunc.bind({ x, y });
+        const boundVolumeFunc = volumeFunc.bind({ x, y, z });
+
+        output.push({
+            area: boundAreaFunc(), volume: boundVolumeFunc()
+        })
+    })
+
+    return output
+}
