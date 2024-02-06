9c9dc3cc4920e77d54201c402b0fdbb2c935f801
George Lazarov <122516587+Moramarth@users.noreply.github.com>
2024-02-06 21:11:31 +0200

    Solution for exercise 7

--- SofitUni JS Advanced/Advanced Functions/exercise_07 Simple Calculator/app.js
+++ SofitUni JS Advanced/Advanced Functions/exercise_07 Simple Calculator/app.js
@@ -1,7 +1,16 @@
 function calculator() {
-    // TODO:
-}
-
-
-
-
+    let firstInput, secondInput, resultBox;
+    return {
+        init: (selector1, selector2, resultSelector) => {
+            firstInput = document.querySelector(selector1);
+            secondInput = document.querySelector(selector2);
+            resultBox = document.querySelector(resultSelector);
+        },
+        add: () => {
+            resultBox.value = Number(firstInput.value) + Number(secondInput.value);
+        },
+        subtract: () => {
+            resultBox.value = Number(firstInput.value) - Number(secondInput.value);
+        },
+    };
+};
