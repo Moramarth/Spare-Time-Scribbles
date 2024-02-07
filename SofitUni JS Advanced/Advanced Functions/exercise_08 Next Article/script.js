a40adc11258ed6f6766cf3a32936860ba27da17c
George Lazarov <122516587+Moramarth@users.noreply.github.com>
2024-02-07 18:49:49 +0200

    Added more solutions

--- SofitUni JS Advanced/Advanced Functions/exercise_08 Next Article/script.js
+++ SofitUni JS Advanced/Advanced Functions/exercise_08 Next Article/script.js
@@ -1,3 +1,16 @@
 function getArticleGenerator(articles) {
-    // TODO
+    const articlesArray = []
+    const contentContainer = document.querySelector('#content')
+    for (const article of articles) {
+        articlesArray.push(article)
+    }
+
+    return () => {
+        if (articlesArray.length !== 0) {
+            const article = document.createElement('article')
+            contentContainer.appendChild(article)
+            article.textContent = articlesArray.shift()
+        }
+    }
+
 }
