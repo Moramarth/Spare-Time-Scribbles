ad4fb4fe95dcac244507d57ed5bb71218f5a4afc
George Lazarov <122516587+Moramarth@users.noreply.github.com>
2025-01-24 08:57:52 +0200

    small refactor

--- SoftUni Python DB/PostgreSQL-January-2025/03-PostgreSQL-Built-in-Functions/exercise_13_positive_and_negative_left.sql
+++ SoftUni Python DB/PostgreSQL-January-2025/03-PostgreSQL-Built-in-Functions/exercise_13_positive_and_negative_left.sql
@@ -1,7 +1,4 @@
 SELECT peak_name,
-       LEFT(peak_name, 4) AS positive_left,
-       LEFT(peak_name, CASE
-                           WHEN LENGTH(peak_name) > 4 THEN LENGTH(peak_name) - 4
-                           ELSE 0
-           END)           AS negative_left
+       LEFT(peak_name, 4)  AS positive_left,
+       LEFT(peak_name, -4) AS negative_left
 FROM peaks;
--- SoftUni Python DB/PostgreSQL-January-2025/03-PostgreSQL-Built-in-Functions/exercise_14_positive_and_negative_right.sql
+++ SoftUni Python DB/PostgreSQL-January-2025/03-PostgreSQL-Built-in-Functions/exercise_14_positive_and_negative_right.sql
@@ -1,7 +1,4 @@
 SELECT peak_name,
-       RIGHT(peak_name, 4) AS positive_right,
-       RIGHT(peak_name, CASE
-                            WHEN LENGTH(peak_name) > 4 THEN LENGTH(peak_name) - 4
-                            ELSE 0
-           END)            AS negative_right
+       RIGHT(peak_name, 4)  AS positive_right,
+       RIGHT(peak_name, -4) AS negative_right
 FROM peaks;
